007fe88b3fc8bcc63fec37592cff94287bff7f5c
ninetailsrabbit <danieldigitalspace@proton.me>
2024-05-31 13:43:22 +0100

    new methods on Board to access cells and tests

--- Match3Maker/src/components/Board.cs
+++ Match3Maker/src/components/Board.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using SystemExtensions;
 
 namespace Match3Maker {
 
@@ -14,6 +15,14 @@ namespace Match3Maker {
 
         public FILL_MODES SelectedFillMode = FILL_MODES.FALL_DOWN;
 
+        #region Events
+        public event Action? PreparedBoard;
+        public event Action? FilledBoard;
+
+        public delegate void ConsumedSequenceEventHandler(Sequence sequence);
+        public event ConsumedSequenceEventHandler? ConsumedSequence;
+        #endregion
+
         public int GridWidth { get => _gridWidth; set { _gridWidth = Math.Max(MIN_GRID_WIDTH, value); } }
         public int GridHeight { get => _gridHeight; set { _gridHeight = Math.Max(MIN_GRID_HEIGHT, value); } }
 
@@ -24,18 +33,23 @@ namespace Match3Maker {
         public Vector2 Offset = new(5, 10);
 
         public List<List<GridCell>> GridCells = [];
+        public List<PieceWeight> AvailablePieces = [];
 
 
+        #region Constructors
         public Board(int gridWidth, int gridHeight) {
             GridWidth = gridWidth;
             GridHeight = gridHeight;
         }
 
+        public static Board Create(int gridWidth, int gridHeight) => new(gridWidth, gridHeight);
+
+        #endregion
+
         #region Information
         public int Dimensions() => GridWidth * GridHeight;
         public Vector2 Size() => new(GridWidth, GridHeight);
         #endregion
-
 
         #region Options
         public Board ChangeGridWidth(int width) {
@@ -75,18 +89,66 @@ namespace Match3Maker {
         }
         #endregion
 
-
         #region Cells
-        public Board PrepareGridCells() {
-            foreach (var column in Enumerable.Range(0, GridWidth)) {
-                GridCells.Add([]);
 
-                foreach (var row in Enumerable.Range(0, GridHeight)) {
-                    GridCells[column].Add(new GridCell(column, row));
+        public GridCell? Cell(Vector2 position) => Cell((int)position.Y, ((int)position.X));
+        public GridCell? Cell(int column, int row) {
+            if (GridCells.Count > 0
+                && column < GridWidth
+                && row < GridHeight
+                && (column & int.MinValue) == 0 && (row & int.MinValue) == 0 // Bitwise operation to check if is not negative
+            )
+                return GridCells[column][row];
+
+            return null;
+        }
+
+        public Vector2 CellPosition(GridCell cell)
+            => new(CellSize.X * cell.Column + (Offset.X * cell.Column), CellSize.Y * cell.Row + (Offset.Y * cell.Row));
+
+        public Board PrepareGridCells(bool overwrite = false) {
+
+            if (GridCells.IsEmpty() || overwrite) {
+                GridCells.Clear();
+
+                foreach (var column in Enumerable.Range(0, GridWidth)) {
+                    GridCells.Add([]);
+
+                    foreach (var row in Enumerable.Range(0, GridHeight)) {
+                        GridCells[column].Add(new GridCell(column, row));
+                    }
                 }
+
+                PreparedBoard?.Invoke();
             }
 
             return this;
+        }
+        public GridCell? UpperCellFrom(GridCell cell) {
+            var upperRow = cell.Row - 1;
+
+            return Cell(cell.Column, upperRow);
+        }
+
+        public GridCell? BottomCellFrom(GridCell cell) {
+            var bottomRow = cell.Row + 1;
+
+            return Cell(cell.Column, bottomRow);
+        }
+
+        public GridCell? RightCellFrom(GridCell cell) {
+            var rightColumn = cell.Column + 1;
+
+            return Cell(rightColumn, cell.Row);
+        }
+
+        public GridCell? LeftCellFrom(GridCell cell) {
+            var leftColumn = cell.Column - 1;
+
+            return Cell(leftColumn, cell.Row);
+        }
+        public List<GridCell> EmptyCells() {
+            return GridCells.SelectMany(cells => cells).Where(cell => cell.IsEmpty()).ToList();
         }
         #endregion
     }
--- Match3Maker/tests/src/components/BoardTests.cs
+++ Match3Maker/tests/src/components/BoardTests.cs
@@ -6,6 +6,19 @@ using Xunit;
 namespace Match3Tests {
 
     public class BoardTests {
+
+        [Fact]
+        public void Can_Be_Created_With_Static_Method_Create() {
+            int width = 5;
+            int height = 6;
+
+            var board = Board.Create(width, height);
+
+            Assert.Equal(width, board.GridWidth);
+            Assert.Equal(height, board.GridHeight);
+            Assert.Equal(width * height, board.Dimensions());
+        }
+
         [Fact]
         public void Grid_Dimensions_Are_Set_Correctly_On_New_Board() {
             int width = 5;
@@ -50,6 +63,82 @@ namespace Match3Tests {
 
             Assert.Equal(board.GridWidth, board.GridCells.Count);
             Assert.Equal(board.Dimensions(), board.GridCells.SelectMany(cells => cells).Count());
+
+            //Should not change current cells if overwrite is false
+            board.ChangeGridSize(new Vector2(10, 10)).PrepareGridCells();
+
+            Assert.Equal(8, board.GridCells.Count);
+            Assert.Equal(8 * 7, board.GridCells.SelectMany(cells => cells).Count());
+        }
+
+        [Fact]
+        public void Should_Be_Able_To_Overwrite_Grid_Cells_When_Size_Is_Changed_And_Overwrite_Is_True() {
+            var board = new Board(8, 7);
+
+            Assert.Empty(board.GridCells);
+
+            board.PrepareGridCells();
+
+            Assert.Equal(board.GridWidth, board.GridCells.Count);
+            Assert.Equal(board.Dimensions(), board.GridCells.SelectMany(cells => cells).Count());
+
+            board.ChangeGridSize(new Vector2(10, 10)).PrepareGridCells(true);
+
+            Assert.Equal(board.GridWidth, board.GridCells.Count);
+            Assert.Equal(board.Dimensions(), board.GridCells.SelectMany(cells => cells).Count());
+        }
+
+        [Fact]
+        public void Should_Raise_Prepared_Board_Event_When_Prepared_Grid_Cells() {
+            var board = new Board(5, 6);
+
+            List<string> receivedEvents = [];
+
+            void listener() {
+                receivedEvents.Add("Prepared");
+            }
+
+            board.PreparedBoard += listener;
+
+            board.PrepareGridCells();
+
+            Assert.Single(receivedEvents);
+            Assert.Equal("Prepared", receivedEvents.First());
+
+            board.PreparedBoard -= listener;
+        }
+
+        [Fact]
+        public void Should_Return_Adjacent_Cells_Or_Null_When_Requested() {
+            var board = new Board(4, 5);
+            board.PrepareGridCells();
+
+            Assert.Null(board.Cell(6, 3));
+            Assert.Null(board.Cell(0, 9));
+            Assert.Null(board.Cell(8, 8));
+
+            GridCell? originCell = board.Cell(1, 2);
+            Assert.NotNull(originCell);
+
+            GridCell upperCell = board.UpperCellFrom(originCell);
+            Assert.True(originCell.IsColumnNeighbourOf(upperCell));
+            Assert.Equal(originCell.Row - 1, upperCell.Row);
+            Assert.Equal(upperCell.Column, originCell.Column);
+
+            GridCell bottomCell = board.BottomCellFrom(originCell);
+            Assert.True(originCell.IsColumnNeighbourOf(bottomCell));
+            Assert.Equal(originCell.Row + 1, bottomCell.Row);
+            Assert.Equal(bottomCell.Column, originCell.Column);
+
+            GridCell rightCell = board.RightCellFrom(originCell);
+            Assert.True(originCell.IsRowNeighbourOf(rightCell));
+            Assert.Equal(originCell.Column + 1, rightCell.Column);
+            Assert.Equal(rightCell.Row, originCell.Row);
+
+            GridCell leftCell = board.LeftCellFrom(originCell);
+            Assert.True(originCell.IsRowNeighbourOf(leftCell));
+            Assert.Equal(originCell.Column - 1, leftCell.Column);
+            Assert.Equal(leftCell.Row, originCell.Row);
         }
     }
 
